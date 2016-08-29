09199e6c094a8f95663f8e8fc572f6b6624df9e6
jellyfishgh <1129088994@qq.com>
2016-08-30 00:16:06 +0800

    ubuntu:ipc in node

--- ipc/master.js
+++ ipc/master.js
@@ -1,1 +1,21 @@
-// http://taobaofed.org/blog/2015/11/10/nodejs-cluster-2/
+const WriteWrap = process.binding('stream_wrap').WriteWrap;
+const cp = require('child_process');
+const path = require('path');
+
+let worker = cp.fork(path.join(__dirname, 'worker.js'));
+let channel = worker._channel;
+
+channel.onread = function (len, buf) {
+    if (buf) {
+        console.log(buf.toString());
+        channel.close();
+    } else {
+        channel.clsoe();
+        console.log('channel closed');
+    }
+};
+
+channel.writeUtf8String(new WriteWrap(), JSON.stringify({
+    'hello': 'worker',
+    'pid': process.pid
+}), null);
